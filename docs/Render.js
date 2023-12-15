861e8e40e2a97c86cae75e2554874835c0672d95
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2023-12-15 11:45:27 -0800

    Create Render.js

---
+++ docs/Render.js
@@ -0,0 +1,5 @@
+const interactiveElement = document.getElementById('interactive-element');
+
+interactiveElement.addEventListener('click', function() {
+    alert('You clicked the interactive element!');
+});
